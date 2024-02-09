e80f52a21927c0de5ca73c9d75d8cb11977aaecb
Blake-sizemore <magnify1more@gmail.com>
2024-02-09 18:12:50 -0500

    final ts set up

--- script.ts
+++ script.ts
@@ -1,5 +1,6 @@
 console.log('working');
-var resume = {
+
+let resume = {
     name: "Blake",
     career: "unknown",
     description: "This where my desction goes",
@@ -29,40 +30,48 @@ var resume = {
         },
     ],
     skills: ['skill 1', 'skill 2', 'skill 3', 'skill 4'],
-};
+
+}
+
 console.log("Name: " + resume.name.toUpperCase());
 console.log("Career: " + resume.career);
 console.log("Desciption: " + resume.description);
+
+
+
 function callIntrest() {
     console.log('My interest:');
-    for (var i = 0; i < resume.interest.length; i++) {
-        var interestNum = resume.interest[i];
+    for (let i = 0; i < resume.interest.length; i++) {
+        const interestNum = resume.interest[i];
         console.log("* " + interestNum);
     }
 }
+
 callIntrest();
+
 function callPre() {
     console.log('My Previous Things:');
-    for (var i = 0; i < resume.past_jobs_arrObj.length; i++) {
-        var skilCompany = resume.past_jobs_arrObj[i].company;
-        var skilTitle = resume.past_jobs_arrObj[i].title;
-        var skilDescription = resume.past_jobs_arrObj[i].description;
+    for (let i = 0; i < resume.past_jobs_arrObj.length; i++) {
+        const skilCompany = resume.past_jobs_arrObj[i].company;
+        const skilTitle = resume.past_jobs_arrObj[i].title;
+        const skilDescription = resume.past_jobs_arrObj[i].description;
+
         console.log("*  " + skilCompany);
         console.log("  *" + skilTitle);
         console.log("     *" + skilDescription);
     }
 }
+
 callPre();
+
 function callSkill() {
     console.log('My skills:');
-    for (var i = 0; i < resume.skills.length; i++) {
-        var skilNum = resume.skills[i];
+    for (let i = 0; i < resume.skills.length; i++) {
+        const skilNum = resume.skills[i];
         if (i % 2) {
             console.log("* BAM: " + skilNum);
-        }
-        else {
-            console.log("* " + skilNum);
-        }
+        } else { console.log("* " + skilNum); }
     }
 }
+
 callSkill();
